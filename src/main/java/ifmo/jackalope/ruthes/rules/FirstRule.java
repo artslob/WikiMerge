08264b0aef1eea638aefa9241c3d60153e587728
artslob <aslob96@yandex.ru>
2018-04-04 00:12:19 +0300

    find_most_similar_sense - сравнивает сенсы с concept; определён вектор дальнейшего развития (в TODO)

--- src/main/java/ifmo/jackalope/ruthes/rules/FirstRule.java
+++ src/main/java/ifmo/jackalope/ruthes/rules/FirstRule.java
@@ -27,36 +27,54 @@ public class FirstRule implements Rule {
             if (source_senses == null || source_senses.size() < 1)
                 continue;
 
-            WikiSense source_sense = null;
-            if (source_senses.size() > 1) {
-                int source_similarity = 0;
-                for (WikiSense sense : source_senses) {
-                    int current_similarity = compare_links(wiki_senses, lemma_to_sense, sense, concept);
-                    if (current_similarity > source_similarity) {
-                        source_sense = sense;
-                        source_similarity = current_similarity;
-                    }
-                }
-                if (source_similarity == 0)
-                    continue;
-            }
-            else source_sense = source_senses.get(0);
+            WikiSense source_sense = find_most_similar_sense(source_senses, wiki_senses, lemma_to_sense, concept);
+            if (source_sense == null)
+                continue;
 
+            // TODO: для sense_option - попробовать определить к какому сенсу идёт ссылка (корректировка текущих связей)
+            // TODO: для связей в concept - попробовать найти аналогичные связи в вики (построение новых)
+            // TODO: для links - попробовать найти аналогичную связь в рутезе и экспортировать связи этого узла в вики (новые для сенса, на который указывает link)
+
+            // это корректировка существующих связей
             for (SenseOption option : source_sense.getAllOptions()){
                 String lemma = option.getOption().toString();
                 SenseOptionType option_type = option.getType();
 
                 List<WikiSense> possible_target_senses = lemma_to_sense.get(lemma);
-                for (WikiSense sense : possible_target_senses) {
+                if (possible_target_senses == null || possible_target_senses.size() < 1)
+                    continue;
 
-                }
+                // TODO: тут надо сравнить концепты/синонимы имеющие связь с текущим concept и possible_target_senses (то есть будут сравниваться их связи, аналогично выше)
+                WikiSense most_similar_target_sense = find_most_similar_sense(possible_target_senses, wiki_senses, lemma_to_sense, );
             }
+
+            // TODO: for links in source_sense
 
 
             System.out.println();
         }
 
         return links_restored;
+    }
+
+    private WikiSense find_most_similar_sense(List<WikiSense> source_senses, Map<String, WikiSense> wiki_senses,
+                                              Map<String, List<WikiSense>> lemma_to_sense, Concept concept)
+    {
+        if (source_senses.size() <= 0)
+            return null;
+        if (source_senses.size() == 1)
+            return source_senses.get(0);
+
+        WikiSense current_sense = null;
+        int similarity = 0;
+        for (WikiSense sense : source_senses) {
+            int current_similarity = compare_links(wiki_senses, lemma_to_sense, sense, concept);
+            if (current_similarity > similarity) {
+                current_sense = sense;
+                similarity = current_similarity;
+            }
+        }
+        return current_sense;
     }
 
     private int compare_links(Map<String, WikiSense> wiki_senses, Map<String, List<WikiSense>> lemma_to_sense,
