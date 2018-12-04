8354d6297c988084439ccd5069d931e0ab513dc7
artslob <artslob@yandex.ru>
2018-12-04 15:36:48 +0300

    javadoc for Relation

--- src/main/java/ifmo/jackalope/ruthes/entries/Relation.java
+++ src/main/java/ifmo/jackalope/ruthes/entries/Relation.java
@@ -1,7 +1,23 @@
 package ifmo.jackalope.ruthes.entries;
 
+/**
+ * Класс, обозначающий связь между 2 понятиями словаря RuThes. Отношение имеет направление и тип.
+ *
+ * Направление:
+ * Объект вида Entry, имеющий набор отношений, является исходым понятием, от которого направлено отношение.
+ * Объект же данного класса содержит ссылку на объект, к которому направлено данное отношение, и тип этого отношения.
+ *
+ * Типы отношений:
+ * @see RelationType
+ */
 public class Relation {
+    /**
+     * Понятие, к которому направлено отношение.
+     */
     private final Concept concept;
+    /**
+     * Тип отношения.
+     */
     private final RelationType relationType;
 
     public Relation(Concept concept, RelationType relationType) {
